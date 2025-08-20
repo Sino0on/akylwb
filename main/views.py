9a91ed20e78922b1b7b76422a88f751d0cb362ef
Dastan <dastan.kubanuulu1@gmail.com>
2025-08-20 13:36:05 +0600

    das

--- main/views.py
+++ main/views.py
@@ -25,6 +25,15 @@ def index(request):
     if request.method == "POST":
         product_id = request.POST.get("product_id")
         if product_id:
+            url = f"https://card.wb.ru/cards/v4/detail?appType=1&dest=286&nm={product_id}"
+            try:
+                r = requests.get(url, timeout=10)
+                r.raise_for_status()
+                data = r.json()
+            except Exception as e:
+                data = {"error": str(e)}
+        product_id = data.get('products', [])[0].get('root', None)
+        if product_id:
             url = f"https://feedbacks2.wb.ru/feedbacks/v2/{product_id}"
             try:
                 r = requests.get(url, timeout=10)
@@ -37,6 +46,16 @@ def index(request):
 
 
 def export_excel(request, product_id: int):
+    if product_id:
+        url = f"https://card.wb.ru/cards/v4/detail?appType=1&dest=286&nm={product_id}"
+        try:
+            r = requests.get(url, timeout=10)
+            r.raise_for_status()
+            data = r.json()
+        except Exception as e:
+            data = {"error": str(e)}
+    product_id = data.get('products', [])[0].get('root', None)
+
     url = f"https://feedbacks2.wb.ru/feedbacks/v2/{product_id}"
     r = requests.get(url, timeout=15)
     r.raise_for_status()
